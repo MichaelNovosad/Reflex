620a7768bfd9c9b348ad2c0c286feb1d7ed829b4
MichaelNovosad <m.novosad@artk.ai>
2022-09-30 13:54:30 +0300

    Setup no-storyboard

---
+++ Reflex/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Reflex
+//
+//  Created by Michael Novosad on 30.09.2022.
+//
+
+import UIKit
+import Networking
+
+class MainViewController: UIViewController {
+
+    let test = Networking.text
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        navigationController?.title = test
+    }
+
+
+}
+
